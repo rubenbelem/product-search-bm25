523d18a4d6cb141fa378ed8165d16f976ce4bb68
Rúben Belém <rubenbelem@gmail.com>
2019-10-29 00:30:25 -0400

    Fixing .gitignore caching

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
-int main() {
-    ofstream out("teste.txt");
-    out << "oi.";
-    std::cout << "Hello, World!" << std::endl;
-    out.close();
+int main( int argc, char *argv[] ) {
+    ifstream productsFile;
+    if (argc < 2) {
+        cout << "The 1st parameter <data_path> must be set!";
+        return 1;
+    }
+
+    productsFile.open(argv[1]);
+
+    if (productsFile.fail()) {
+        cout << "The products file was not found.";
+        return 1;
+    }
+
+    string line;
+
+    while(getline(productsFile, line)) {
+        cout << line << endl;
+    }
+
+    productsFile.close();
     return 0;
 }
