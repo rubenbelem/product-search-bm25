df3c592de29138c634fcae585e8ab453bf0d2da0
Rúben Belém <rubenbelem@gmail.com>
2019-10-29 00:56:05 -0400

    Reading and parsing json input file

---
+++ src/Product.h
@@ -0,0 +1,9 @@
+#ifndef TECHNICAL_TEST_LINX_PRODUCT_H
+#define TECHNICAL_TEST_LINX_PRODUCT_H
+
+struct Product {
+    std::string id;
+    std:: string name;
+};
+
+#endif //TECHNICAL_TEST_LINX_PRODUCT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include "../deps/json/json.hpp"
+#include "Product.h"
 
 using namespace std;
+using nlohmann::json;
 
 int main( int argc, char *argv[] ) {
     ifstream productsFile;
@@ -21,7 +24,12 @@ int main( int argc, char *argv[] ) {
     string line;
 
     while(getline(productsFile, line)) {
-        cout << line << endl;
+        Product product;
+        auto productJson = json::parse(line);
+        productJson.at("id").get_to(product.id);
+        productJson.at("name").get_to(product.name);
+
+        cout << product.id << " | " << product.name << endl;
     }
 
     productsFile.close();
